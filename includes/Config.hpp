570a369f18b30a38ec47e132638b9ba7284dc0f2
pettei47 <kitagawa.teppei.33c@gmail.com>
2022-12-29 16:22:17 +0900

    config parseもどき

--- includes/Config.hpp
+++ includes/Config.hpp
@@ -1,9 +1,9 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
-// #include "Parser.hpp"
-// #include "Server.hpp"
-// #include "Utility.hpp"
+#include "Parser.hpp"
+#include "Server.hpp"
+#include "Utility.hpp"
 #include "WebServ.hpp"
 #include <iostream>
 #include <map>
@@ -18,20 +18,20 @@ class Config
     void      _parse_server(std::string src, size_t line_start, size_t line_end);
     void      _parse_server_property(std::string src, size_t line, Server& server);
     void      _parse_location_property(std::string src, size_t line, Location& location);
-    void      _validate_config(void);
-    void      _complete_config(void);
-    Server    _default_server(void);
-    Location  _default_Location(void);
+    void      _validate_config();
+    void      _complete_config();
+    Server    _default_server();
+    Location  _default_Location();
     Location  _parse_location(std::string src, size_t line_start, size_t line_end);
     bool      _check_redirect_status(std::string stat);
   public:
     std::vector<Server> _servers;
-    Config(void);
-    Config(std::string file);
+    Config();
+    Config(std::string filepath);
     Config& oparator=(const Config& other);
     ~Config(void);
-    std::vector<Server> get_servers(void);
-    void  show_server_info(void);
+    std::vector<Server> get_servers();
+    void  show_server_info();
 };
 
 
---
+++ srcs/Config.cpp
@@ -0,0 +1,114 @@
+#include "Config.hpp"
+
+Config::Config() {}
+
+Config::Config(std::string filepath)
+{
+  _parse_config(filepath);
+  _complete_config();
+}
+
+Config& Config::operator=(const Config& other)
+{
+  this->_servers = other._servers;
+  return (*this);
+}
+
+Config::~Config() {}
+
+/**
+ * @brief confファイルをパースする
+ * @param filepath: string 設定ファイルの相対パス
+ */
+
+void  Config::_parse_config(std::stiring filepath)
+{
+  std::string               file_content;
+  size_t                    i;
+  size_t                    size;
+  std::vector<std::string>  line;
+
+  i = 0;
+  file_content = read_file(filepath);
+  size = count_lines(file_content);
+  for (size_t i = 0, i < size - 1, i++)
+  {
+    if (!is_skip(file_content, i))
+    {
+      line = split_white_space(get_line(file_content, i));
+      if(line.size() > 0 && line[0] == "server")
+      {
+        _parse_server(file_content, i , get_closing_bracket(file_content, i));
+        i = get_closing_bracket(file_content, i);
+      }
+      else
+        throw ParsingException(i, "Unexpected param '" + line[0] + "'.");
+    }
+  }
+  _validate_config();
+}
+
+/**
+ * @brief confファイルで定義されたserver構造体を配列に追加する
+ * @param src: string confファイルの文字列
+ * @param start: size_t confファイルの開始位置 == "server {"
+ * @param end: size_t confファイルの終了位置 == "}"
+ */
+void  Config::_parse_server(std::string src, size_t start, size_t end)
+{
+  Server  server = _default_server();
+
+  for(size_t i = start + 1, i < end, ++i)
+  {
+    if(!is_skip(src, i))
+    {
+      std::vector<std::string> words = split_white_space(get_line(src, i));
+      if(words.size() > 0 && words[0] == "location")
+      {
+        server.locations.push_back(_parse_location(src, i, get_closing_bracket(src, i)));
+        i = get_closing_bracket(src, i);
+      }
+      else
+        _parse_server_property(src, i, server);
+    }
+  }
+  _servers.push_back(server);
+}
+
+/**
+ * @brief confファイルからserverのパラメーターを取得し設定する
+ * @param src: string confファイルの文字列
+ * @param i: server contextの開始位置
+ * @param sever: server構造体
+ */
+void  Config::_parse_server_property(std:;string, src, size_t start, Server server)
+{
+  std::vector< std::string > line = parse_property(src, start, "server");
+
+  if(line[0] == server_propaties[0]) // 配列の箇所で扱う値が決まっているなら、ハードコードでもよさそう？
+  {
+    if(line.size() != 3)
+      throw ParsingException(i, std::string(server_properties[0]) + " <port> <host>;");
+    server.port = convert_to_size_t(line[1], start);
+    // line[1]が数字ではないときのチェックがなさそう？
+    server.host = line[2];
+    // line[2]がhostに適さない値のときのチェックがなさそう？
+  }
+  if(line[0] == server_properties[1])
+  {
+    for(size_t i = 1; i < line.size(); ++i)
+      server.names.push_back(line[i]);
+  }
+  if(line[0] == server_properties[2])
+  {
+    if(line.size() != 3)
+      throw ParsingException(n, std::string(server_properties[2]) + "<code> <file>;");
+    server.error_pages[convert_to_size_t(line[1], start)] = line[2];
+    // line[1]が数値にできないとき死にそう
+  }
+  if(line[0] == server_properties[3])
+  {
+    // line.size() < 2のとき死にそう
+    server.root = line[1];
+  }
+}
