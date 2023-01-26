807208feba8c66f3cdb9310c9e299fd65e84e0c5
pettei47 <kitagawa.teppei.33c@gmail.com>
2023-01-26 21:56:49 +0900

    HttpInfo

--- includes/HttpInfo.hpp
+++ includes/HttpInfo.hpp
@@ -10,15 +10,15 @@
 
 struct HttpInfo
 {
-	std::string method;
-	std::string uri;
-	std::string script_name;
-	std::string query_string;
-	std::string protocol_version;
-	Header      headers;
-	HttpString	body;
-	Server*     server;
-	Location*   location;
+  std::string method;
+  std::string uri;
+  std::string script_name;
+  std::string query_string;
+  std::string protocol_version;
+  Header      headers;
+  HttpString  body;
+  Server*     server;
+  Location*   location;
 };
 
 #endif /* HTTP_INFO_CPP */
