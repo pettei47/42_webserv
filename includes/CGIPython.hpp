c4b2086468a02023de82dbafa3799a13fde511c8
pettei47 <kitagawa.teppei.33c@gmail.com>
2023-01-29 01:47:34 +0900

    indentをspaceに統一

--- includes/CGIPython.hpp
+++ includes/CGIPython.hpp
@@ -18,61 +18,61 @@
 class CGIPython
 {
 private:
-	enum cgiphase
-	{
-		ENV,
-		ARG,
-		PIPE,
-		PROCESS
-	};
+  enum cgiphase
+  {
+    ENV,
+    ARG,
+    PIPE,
+    PROCESS
+  };
 
-	static const int BUF_SIZE = 8192;
+  static const int BUF_SIZE = 8192;
 
-	int _write_fd;
-	int _read_fd;
-	pid_t _ch_pid;
-	HttpString _data;
-	Status& _httpstatus;
-	Header& _httpheader;
-	Body& _body;
-	HttpInfo& _info;
-	std::string& _filepath;
-	TimeManage _time;
-	size_t _sent_size;
+  int           _write_fd;
+  int           _read_fd;
+  pid_t         _ch_pid;
+  HttpString    _data;
+  Status&       _httpstatus;
+  Header&       _httpheader;
+  Body&         _body;
+  HttpInfo&     _info;
+  std::string&  _filepath;
+  TimeManage    _time;
+  size_t        _sent_size;
 
-	void _check_python();
-	void _check_execute_file();
-	void _set_env(EnvVar& env);
-	void _make_arg(char*** arg);
-	void _set_pipe(int pipe_c2p[2], int pipe_p2c[2]);
-	void _make_process();
-	void _delete_resources(char** arg, int* pipe_c2p, int* pipe_p2c, int flag);
-	void _child_process(int pipe_c2p[2], int pipe_p2c[2], char** arg, char** env);
-	void _set_fd(int pipe_c2p[2], int pipe_p2c[2]);
-	void _first_preparation_delete(char** env, char** arg);
-	void _read();
-	void _write();
-	void _read_or_write_error(int status);
-	void _kill_process();
+  void  _check_python();
+  void  _check_execute_file();
+  void  _set_env(EnvVar& env);
+  void  _make_arg(char*** arg);
+  void  _set_pipe(int pipe_c2p[2], int pipe_p2c[2]);
+  void  _make_process();
+  void  _delete_resources(char** arg, int* pipe_c2p, int* pipe_p2c, int flag);
+  void  _child_process(int pipe_c2p[2], int pipe_p2c[2], char** arg, char** env);
+  void  _set_fd(int pipe_c2p[2], int pipe_p2c[2]);
+  void  _first_preparation_delete(char** env, char** arg);
+  void  _read();
+  void  _write();
+  void  _read_or_write_error(int status);
+  void  _kill_process();
 
 public:
-	CGIPython(HttpInfo& info,
-			  Status& httpstatus,
-			  Header& httpheader,
-			  Body& body,
-			  std::string& filepath);
-	~CGIPython();
+  CGIPython(HttpInfo& info,
+        Status& httpstatus,
+        Header& httpheader,
+        Body& body,
+        std::string& filepath);
+  ~CGIPython();
 
-	enum phase first_preparation();
-	void set_select_fd(fd_set& read_set, fd_set& write_set, int& max_fd) const;
-	enum phase check_and_handle(fd_set& read_set, fd_set& write_set);
+  enum phase  first_preparation();
+  void        set_select_fd(fd_set& read_set, fd_set& write_set, int& max_fd) const;
+  enum phase  check_and_handle(fd_set& read_set, fd_set& write_set);
 
-	HttpString get_data() const;
+  HttpString  get_data() const;
 
 private:
-	CGIPython();
-	CGIPython(CGIPython const& other);
-	CGIPython& operator=(CGIPython const& other);
+  CGIPython();
+  CGIPython(CGIPython const& other);
+  CGIPython& operator=(CGIPython const& other);
 };
 
 #endif
--- srcs/Autoindex.cpp
+++ srcs/Autoindex.cpp
@@ -6,55 +6,55 @@ Autoindex::~Autoindex() { }
 
 std::string Autoindex::generate_body(std::string script_name, std::string dirpath)
 {
-	DIR* d = _open_directory(dirpath);
-	std::vector< std::string > entries = _get_file_entries(d);
-	return _generate_html_body(script_name, entries);
+  DIR* d = _open_directory(dirpath);
+  std::vector< std::string > entries = _get_file_entries(d);
+  return _generate_html_body(script_name, entries);
 }
 
 std::string Autoindex::_generate_html_body(std::string script_name,
-										   std::vector< std::string > const& entries)
+                        std::vector< std::string > const& entries)
 {
-	std::string content;
-	content = "<h1>Index of " + script_name;
-	content += "</h1><hr>" + ft::CRLF;
-	content += "<pre><a href=\"../\">../</a>" + ft::CRLF;
+  std::string content;
+  content = "<h1>Index of " + script_name;
+  content += "</h1><hr>" + ft::CRLF;
+  content += "<pre><a href=\"../\">../</a>" + ft::CRLF;
 
-	size_t size = entries.size();
-	for(size_t i = 0; i < size; i++)
-	{
-		content += "<a href=\"";
-		content += entries[i];
-		content += "\">";
-		content += entries[i];
-		content += "</a>" + ft::CRLF;
-	}
-	content += "</pre><hr>";
-	return content;
+  size_t size = entries.size();
+  for(size_t i = 0; i < size; i++)
+  {
+    content += "<a href=\"";
+    content += entries[i];
+    content += "\">";
+    content += entries[i];
+    content += "</a>" + ft::CRLF;
+  }
+  content += "</pre><hr>";
+  return content;
 }
 
 std::vector< std::string > Autoindex::_get_file_entries(DIR* d)
 {
-	std::vector< std::string > entries;
-	struct dirent* entry;
-	std::string name;
-	while((entry = readdir(d)))
-	{
-		name = entry->d_name;
-		if(name[0] == '.')
-			continue;
-		if(entry->d_type == DT_DIR)
-			name += '/';
-		entries.push_back(name);
-	}
-	return entries;
+  std::vector< std::string > entries;
+  struct dirent* entry;
+  std::string name;
+  while((entry = readdir(d)))
+  {
+    name = entry->d_name;
+    if(name[0] == '.')
+      continue;
+    if(entry->d_type == DT_DIR)
+      name += '/';
+    entries.push_back(name);
+  }
+  return entries;
 }
 
 DIR* Autoindex::_open_directory(std::string dirpath)
 {
-	DIR* d = opendir(dirpath.c_str());
-	if(!d)
-	{
-		throw http::StatusException(403);
-	}
-	return d;
+  DIR* d = opendir(dirpath.c_str());
+  if(!d)
+  {
+    throw http::StatusException(403);
+  }
+  return d;
 }
